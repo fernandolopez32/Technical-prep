04dc938b99710d55d3f17a7fd3201e8c14ca17a8
Fernando Lopez <flopez00032@gmail.com>
2023-01-14 14:09:40 -0600

    solved number 2

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,20 @@
 
 //loop that counts to a given number
 function countToNumber(number){
-    for(let i =0; i<=number; i++){
+    for(let i =1; i<=number; i++){
         console.log(i)
     }
 }
-countToNumber(50);
+//solved
+// countToNumber(50);
 
 //given a sorted array of integers arr and an integer target, find the index of the first and last position of target in arr. Ir target cant be found in arr, return [-1, -1]
 
 let arr = [1,2,4,5,6,7,5,4,3,4]
 let target = 5;
 
-function findFirstAndLastInstanceOfTarget(array){
-
+function findFirstAndLastInstanceOfTarget(array) {
+    return (`[${array.indexOf(target)},${array.lastIndexOf(target)}]`);
 }
+
+console.log(findFirstAndLastInstanceOfTarget(arr));
